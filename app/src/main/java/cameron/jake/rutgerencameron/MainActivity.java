64900b5df986160db2bf37f2daf19350ac5db739
Rutger Nijhuis <rutger.nijhuis@gmail.com>
2017-05-11 13:09:47 +0200

    added stuff:

--- app/src/main/java/cameron/jake/rutgerencameron/MainActivity.java
+++ app/src/main/java/cameron/jake/rutgerencameron/MainActivity.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private int lol;
+    private int wow;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lol = 10;
+        wow = lol*lol;
+
     }
+
+
+
 }
